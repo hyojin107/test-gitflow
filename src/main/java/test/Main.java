e532f80217bb90d04cbc1329b570e40f85265c82
Hyojin Song <49690185+hyojin107@users.noreply.github.com>
2021-02-04 00:56:19 +0900

    [FEATURE] println 추가 (#2) (#3)
    * feat: hi22
    
    * feat: 마지막 문장 제거
    
    * feat: 3333으로 되돌림

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hi222");
+        System.out.println("33333333333");
     }
 }
