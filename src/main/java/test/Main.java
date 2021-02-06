1e3bb1756d34380810754cd11879de21417114c5
Hyojin Song <49690185+hyojin107@users.noreply.github.com>
2021-02-06 14:22:44 +0900

    Develop  (#15)
    * [FEATURE] println 추가 (#2)
    
    * feat: hi22
    
    * feat: 마지막 문장 제거
    
    * feat: 3333으로 되돌림
    
    * feat: ---추가
    
    * feat: Gather작성 (#13)
    
    * Feature/#9 gather println (#14)
    
    * feat: Gather작성
    
    * feat: 출력문 수정
    
    * feat: 충돌 해결중

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -6,5 +6,14 @@ public class Main {
         System.out.println("hello");
         System.out.println("hi222");
         System.out.println("33333333333");
+
+        System.out.println("-----------------------------");
+
+
+
+        System.out.println("Gather~~~!!");
+
+        System.out.println("feature9");
+
     }
 }
